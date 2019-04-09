b6f91519ab80d17ae151c4e768d3fbe6254cd720
kitfactory <kitfactory@gmail.com>
2019-04-09 20:28:07 +0900

    update

---
+++ fp16.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+# from keras.datasets import mnist
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout, Flatten
+# from keras.layers import Conv2D, MaxPooling2D
+
+tf.keras.backend.set_floatx('float16')
+tf.keras.backend.set_epsilon(1e-4)
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+if tf.keras.backend.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float16')
+x_test = x_test.astype('float16')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Dropout(0.25))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
+
+model.compile(loss=tf.keras.losses.categorical_crossentropy,
+              optimizer=tf.keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
--- mnist_113.py
+++ mnist_113.py
@@ -3,7 +3,10 @@ from __future__ import absolute_import, division, print_function
 import tensorflow as tf
 import numpy as np
 
-# tf.enable_eager_execution()
+from typing import ClassVar
+
+import os
+os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
 
 
 # config = tf.ConfigProto()
@@ -12,92 +15,89 @@ import numpy as np
 sess = tf.Session()
 sess.as_default()
 
+dtype='float16'
+tf.keras.backend.set_floatx('float16')
+# default is 1e-7 which is too small for float16.  Without adjusting the epsilon, we will get NaN predictions because of divide by zero problems
+tf.keras.backend.set_epsilon(1e-4) 
+
+
 class Mixup():
 
-    def __init__(self, keeps: int, classes: int, img_size:int, channel:int , alpha: float):
-        """初期化をする。
-        
-        Arguments:
-            keeps {int} -- MIXするために保持しておく数
-            classes {int} -- ラベルのクラス数
-            alpha {float} -- mixupするときのハイパーパラメータ
-        """
+    def __init__(self):
+        super().__init__()
+
+    def config(self, keeps: int, classes: int, img_size:int, channel:int , alpha: float):
         self.mixup_keeps = keeps
         self.num_class = classes
         self.alpha = alpha
         self.mixup_flag = tf.Variable(tf.zeros([self.mixup_keeps], dtype=tf.bool), dtype=tf.bool, use_resource=True)
         self.mixup_label = tf.Variable(tf.zeros([self.mixup_keeps, self.num_class], dtype=tf.float32), dtype=tf.float32, use_resource=True)
-        # self.mixup_img = tf.Variable(tf.zeros([self.mixup_keeps,img_size, img_size, channel], dtype=tf.float32), dtype=tf.float32, use_resource= True)
-        self.beta_dist = tf.distributions.Beta(alpha, alpha)
+        self.mixup_img = tf.Variable(tf.zeros([self.mixup_keeps,img_size, img_size, channel], dtype=tf.float32), dtype=tf.float32, use_resource= True)
+        self.beta_dist = tf.distributions.Beta(self.alpha, self.alpha)
 
-    # def mixup_true_func(self, idx, img, label):
-    def mixup_true_func(self, idx, label):
-        """mixupでキープしておいたラベルを返却する。
+    def mixup_dataset(self, dataset:tf.data.Dataset):
+
+        # def mixup_true_func(self, idx, img, label):
+        def mixup_true_func(idx, img, label):
+            """mixupでキープしておいたラベルを返却する。
+
+            Arguments:
+                idx {integer} -- キープしていたラベルのインデックス
+               label {tf.Tensor} -- 読み込んだデータのラベル値
     
-        Arguments:
-            idx {integer} -- キープしていたラベルのインデックス
-            label {tf.Tensor} -- 読み込んだデータのラベル値
+            Returns:
+                tf.Tensor -- インデックスに保持されていたOne-Hot済みのラベルの値
+            """
+            # tf.Print(idx,[idx], 'In True Func')
+            ret_img = tf.gather(self.mixup_img, indices=[[idx]])
+            ret_label = tf.gather(self.mixup_label,indices=[[idx]])
+            return ret_img, ret_label
+
+        # def mixup_false_func(self, idx, img, label):
+        def mixup_false_func(idx, img, label):
+            """mixupでラベル値をキープする。
+
+            Arguments:
+                idx {integer} -- キープして
+                label {[type]} -- [description]
     
-        Returns:
-            tf.Tensor -- インデックスに保持されていたOne-Hot済みのラベルの値
-        """
-        tf.Print(idx,[idx], 'In True Func')
-        # ret_img = tf.gather(mixup_img, indices=[[idx]])
-        ret_label = tf.gather(mixup_label,indices=[[idx]])
-        # return ret_img, ret_label
-        return ret_label
-
-
-    # def mixup_false_func(self, idx, img, label):
-    def mixup_false_func(self, idx, label):
-        """mixupでラベル値をキープする。
+            Returns:
+                [type] -- [description]
+            """
+            # tf.Print(idx,[idx], 'In False Func')
+            tf.scatter_update(self.mixup_flag,indices=[[idx]],updates=[[True]])
+            return img, label
     
-        Arguments:
-            idx {integer} -- キープして
-            label {[type]} -- [description]
-    
-        Returns:
-            [type] -- [description]
-        """
-        tf.Print(idx,[idx], 'In False Func')
-        tf.scatter_update(mixup_flag,indices=[[idx]],updates=[[True]])
-        # return img, label
-        return label
+        def map_func(img, label):
+            rnd = tf.random.uniform(shape=[1],minval=0,maxval=self.mixup_keeps,dtype=tf.int32)
+            idx = tf.reshape(rnd,[])
+            z = tf.gather(self.mixup_flag,[[idx]])
+            z = tf.reshape(z,[])
 
-    def map_func(self, label):
-        rnd = tf.random.uniform(shape=[1],minval=0,maxval=self.num_class,dtype=tf.int32)
-        idx = tf.reshape(rnd,[])
-        z = tf.gather(mixup_flag,[[idx]])
-        z = tf.reshape(z,[])
+            img2, label2 = tf.cond(z,true_fn=lambda: mixup_true_func(idx,img, label), false_fn=lambda: mixup_false_func(idx,img, label))
 
-        img2, label2 = tf.cond(z,true_fn=lambda: self.mixup_true_func(idx,label),false_fn=lambda: self.mixup_false_func(idx,label))
+            with tf.control_dependencies([idx,label2]):
+                tf.scatter_update(self.mixup_img,indices=[[idx]], updates=[[img2]])
+                tf.scatter_update(self.mixup_label,indices=[[idx]], updates=[[label2]])
 
-        with tf.control_dependencies([idx,label2]):
-            tf.scatter_update(mixup_label,indices=[[idx]], updates=[[label]])
-        lambda_param = beta_dist.sample()
-        with tf.control_dependencies([label2,lambda_param]):
-            # new_img = (lambda_param*img) + (1-lambda_param)*img2
-            new_label = (lambda_param * label) + (1-lambda_param)*label2
-    
-        return new_label
+            lambda_param = self.beta_dist.sample()
+            with tf.control_dependencies([label2,lambda_param]):
+                new_img = (lambda_param*img) + (1-lambda_param)*img2
+                new_label = (lambda_param * label) + (1-lambda_param)*label2
+                return new_img, new_label
 
+        return dataset.map(map_func=map_func)
 
-# tf.
-# config.gpu_options.allow_growth = True
-# config = tf.ConfigProto()
-
-
-# config = tf.compat.v1.ConfigProto()
-# config.gpu_options.allow_growth = True
-# sess = tf.compat.v1.Session(config=config)
 
 
 (x_train, y_train),(x_test,y_test)=tf.keras.datasets.mnist.load_data()
 
 x_train = x_train / 255
-x_train = x_train.astype(np.float32)
+# x_train = x_train.astype(np.float32)
+x_train = x_train.astype(np.float16)
 
-y_train = tf.keras.utils.to_categorical(y_train, num_classes=10, dtype='float32')
+# y_train = tf.keras.utils.to_categorical(y_train, num_classes=10, dtype='float32')
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=10, dtype='float16')
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 
 z = tf.zeros([10], dtype=tf.bool)
@@ -107,36 +107,13 @@ mixup_flag = tf.Variable( tf.zeros(shape=[10],dtype=tf.bool), dtype=tf.bool, use
 # mixup_img_memo = tf.get_variable("img_memo", [10,28,28,1], dtype=tf.float32, initializer=tf.zeros_initializer)
 
 iz = np.zeros([28,28],dtype=np.float32)
-mixup_img_memo = tf.Variable(iz, dtype=tf.float32,name='img_memo')
+# mixup_img_memo = tf.Variable(iz, dtype=tf.float32,name='img_memo')
+mixup_img_memo = tf.Variable(iz, dtype=tf.float16,name='img_memo')
 counter = tf.Variable(0, dtype=tf.int32)
 
 # mixup_label_memo = tf.Variable(tf.zeros([1,10], dtype=tf.float32))
 # mix-up randomをだして、そのidに入れる。
 
-r = tf.Variable(0, tf.int32)
-print('r', r)
-
-hoge = tf.assign(r, 10)
-print('r', r)
-
-
-mixup = MixupAugmentation(keeps=5, classes=10, img_size=28, alpha=0.5)
-
-
-def mix_up(x, y):
-    rnd = tf.random.uniform(shape=[1], dtype=tf.int32,minval=0,maxval=10)
-    ridx = tf.reshape(rnd,[])
-    flgs = mixup_flag
-    flg = tf.gather(flgs, indices=[[5]])
-    return x, y
-
-    # c = counter + 1
-    # tf.assign(counter, c)
-    # with tf.control_dependencies([ridx]):
-    # tf.scatter_update(mixup_img_memo,indices=[[0]],updates=[x])]
-    # print(mixup_img_memo)
-    # print(x)
-    # tf.assign(mixup_img_memo,img)
 
 def map_fn(x,y):
     x = tf.reshape(x,(-1,28,28,1))
@@ -145,10 +122,11 @@ def map_fn(x,y):
 
 # with tf.device('/cpu:0'):
 dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+# mixup = Mixup()
+# mixup.config(keeps=5, classes=10, img_size=28, channel=1, alpha=0.5)
+# dataset = mixup.mixup_dataset(dataset)
 dataset = dataset.batch(100)
-
-
-dataset = dataset.repeat(1) # .map(map_fn,num_parallel_calls=4)
+dataset = dataset.repeat() # .map(map_fn,num_parallel_calls=4)
 
 print(dataset)
 
@@ -164,13 +142,13 @@ print(dataset)
 # sess.run(tf.global_variables_initializer())
 # sess.run(iterator.initializer)
 
-
-# c = 0
-# while True:
-#     c = c + 1
-#     img, label = sess.run(next)
-    # print(c,label)
-
+# try:
+#    while True:
+        # img, label = sess.run(next)
+#         img, label = sess.run(next)
+# except tf.errors.OutOfRangeError as err:
+#    print('End')
+# 
 # exit()
 
 '''
